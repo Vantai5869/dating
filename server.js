4909c8bf234387a4bbc06449ecf84966d90c93f8
taipv <phamvantai2202@gmail.com>
2022-07-09 18:53:30 +0700

    sss

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ mongoose.connect(URI, {
 if(process.env.NODE_ENV === 'production'){
     app.use(express.static('client/build'))
     app.get('*', (req, res) => {
-        res.sendFile(path.join('client', 'build', 'index.html'))
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
     })
 }
 
